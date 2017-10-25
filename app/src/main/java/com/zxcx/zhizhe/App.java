aab46ff8873ca619ac8b29d0281671e596897177
MarkFrank01 <1123212438@qq.com>
2017-10-25 13:08:39 +0800

    提交

---
+++ app/src/main/java/com/zxcx/zhizhe/App.java
@@ -0,0 +1,81 @@
+package com.zxcx.zhizhe;
+
+import android.content.Context;
+import android.support.multidex.MultiDexApplication;
+import android.support.v7.app.AppCompatDelegate;
+
+import com.kingja.loadsir.core.LoadSir;
+import com.meituan.android.walle.WalleChannelReader;
+import com.mob.MobSDK;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
+import com.tencent.bugly.crashreport.CrashReport;
+import com.zxcx.zhizhe.loadCallback.EmptyCallback;
+import com.zxcx.zhizhe.loadCallback.LoadingCallback;
+import com.zxcx.zhizhe.loadCallback.NetworkErrorCallback;
+import com.zxcx.zhizhe.ui.MainActivity;
+import com.zxcx.zhizhe.utils.Constants;
+
+import cn.jiguang.analytics.android.api.JAnalyticsInterface;
+import cn.jpush.android.api.JPushInterface;
+
+
+public class App extends MultiDexApplication {
+    private static Context context;
+    public static App app;
+    private RefWatcher refWatcher;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        app = this;
+        context = getApplicationContext();
+
+        //SMSSDK,ShareSDK
+        MobSDK.init(context, "2056f7654d8f4", "4f929b988ab98c88d4e9811e5a9d208c");
+
+        //Bugly
+        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(context);
+        strategy.setAppChannel(WalleChannelReader.getChannel(context));
+        Beta.canShowUpgradeActs.add(MainActivity.class);
+        Bugly.init(context, "9e3ef1932f", true, strategy);
+
+        //极光推送
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(context);
+
+        //极光统计
+        JAnalyticsInterface.setDebugMode(true);
+        JAnalyticsInterface.init(context);
+
+        //LeakCanary
+        refWatcher = LeakCanary.install(this);
+
+        //夜间模式
+        if (Constants.IS_NIGHT){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
+        //LoadSir
+        LoadSir.beginBuilder()
+                .addCallback(new NetworkErrorCallback())
+                .addCallback(new EmptyCallback())
+                .addCallback(new LoadingCallback())
+                .setDefaultCallback(LoadingCallback.class)
+                .commit();
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+
+    public static RefWatcher getRefWatcher(Context context) {
+        App application = (App) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
+}
