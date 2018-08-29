7703b1b43f0e628539fdeb85556c5f6f4b6b5fdd
MarkFrank01 <1123212438@qq.com>
2018-08-29 10:58:40 +0800

    广告带入token，点击跳转创作

--- app/src/main/java/com/zxcx/zhizhe/ui/welcome/WebViewActivity.kt
+++ app/src/main/java/com/zxcx/zhizhe/ui/welcome/WebViewActivity.kt
@@ -3,12 +3,15 @@ package com.zxcx.zhizhe.ui.welcome
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import com.zxcx.zhizhe.R
 import com.zxcx.zhizhe.mvpBase.BaseActivity
 import com.zxcx.zhizhe.ui.card.share.ShareDialog
-import com.zxcx.zhizhe.utils.ScreenUtils
-import com.zxcx.zhizhe.utils.WebViewUtils
+import com.zxcx.zhizhe.ui.my.creation.CreationAgreementDialog
+import com.zxcx.zhizhe.ui.my.creation.newCreation.CreationEditorActivity
+import com.zxcx.zhizhe.ui.my.writer_status_writer
+import com.zxcx.zhizhe.utils.*
 import kotlinx.android.synthetic.main.activity_web_view.*
 import kotlinx.android.synthetic.main.toolbar.*
 
@@ -21,6 +24,7 @@ class WebViewActivity : BaseActivity(), ADMoreWindow.ADMoreListener {
 	internal var mWebView: WebView? = null
 	private var title = ""
 	private var url = ""
+	private var loadUrl = ""
 	private var imageUrl: String? = null
 	private lateinit var adMoreWindow: ADMoreWindow
 
@@ -38,6 +42,29 @@ class WebViewActivity : BaseActivity(), ADMoreWindow.ADMoreListener {
 		mWebView = WebViewUtils.getWebView(this)
 
 		ll_web_view.addView(mWebView)
+		//添加方法给js调用
+		mWebView?.addJavascriptInterface(this, "native")
+	}
+
+	@JavascriptInterface
+	fun enterCreate() {
+		runOnUiThread {
+			when (SharedPreferencesUtil.getInt(SVTSConstants.writerStatus, 0)) {
+				writer_status_writer -> {
+					//创作界面
+					mActivity.startActivity(CreationEditorActivity::class.java) {}
+					finish()
+				}
+				else -> {
+					val dialog = CreationAgreementDialog()
+					dialog.mListener = {
+						mActivity.startActivity(CreationEditorActivity::class.java) {}
+						finish()
+					}
+					dialog.show(mActivity.supportFragmentManager, "")
+				}
+			}
+		}
 	}
 
 	private fun getIntentData() {
@@ -45,6 +72,11 @@ class WebViewActivity : BaseActivity(), ADMoreWindow.ADMoreListener {
 		url = intent.getStringExtra("url")
 		imageUrl = intent.getStringExtra("imageUrl")
 		val isAD = intent.getBooleanExtra("isAD", false)
+		loadUrl = if (isAD && SharedPreferencesUtil.getInt(SVTSConstants.userId, 0) != 0) {
+			url + "?=" + SharedPreferencesUtil.getString(SVTSConstants.token, "")
+		} else {
+			url
+		}
 		initToolBar(title)
 		iv_toolbar_right.visibility = if (isAD) View.VISIBLE else View.GONE
 		iv_toolbar_right.setImageResource(R.drawable.iv_toolbar_more)
@@ -52,7 +84,7 @@ class WebViewActivity : BaseActivity(), ADMoreWindow.ADMoreListener {
 			adMoreWindow.showAsDropDown(iv_toolbar_right, 0, -ScreenUtils.dip2px(20f))
 		}
 
-		mWebView?.loadUrl(url)
+		mWebView?.loadUrl(loadUrl)
 	}
 
 	override fun onBackPressed() {
