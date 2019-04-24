09073ccf5e9da3bbf47ac34bf9fd3452779ca928
MarkFrank01 <1123212438@qq.com>
2019-04-24 14:33:16 +0800

    将所有的标签选择调整至一致

--- app/src/main/java/com/zxcx/zhizhe/ui/my/creation/newCreation/SelectClassifyAdapter.kt
+++ app/src/main/java/com/zxcx/zhizhe/ui/my/creation/newCreation/SelectClassifyAdapter.kt
@@ -1,10 +1,12 @@
 package com.zxcx.zhizhe.ui.my.creation.newCreation
 
+import android.widget.CheckBox
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.zxcx.zhizhe.R
 import com.zxcx.zhizhe.ui.my.selectAttention.ClassifyBean
 import com.zxcx.zhizhe.utils.ScreenUtils
+import com.zxcx.zhizhe.utils.getColorForKotlin
 
 
 /**
@@ -26,15 +28,25 @@ class SelectClassifyAdapter(data: List<ClassifyBean>) : BaseQuickAdapter<Classif
     override fun convert(helper: BaseViewHolder, item: ClassifyBean) {
 
 //        if (item.isHit == 1) {
-            helper.addOnClickListener(R.id.fl_item_select_card_bag)
-            val para = helper.itemView.layoutParams
-            val screenWidth = ScreenUtils.getDisplayWidth() //屏幕宽度
+        helper.addOnClickListener(R.id.fl_item_select_card_bag)
+        val para = helper.itemView.layoutParams
+        val screenWidth = ScreenUtils.getDisplayWidth() //屏幕宽度
 //		para.width = (screenWidth - ScreenUtils.dip2px((15 * 2).toFloat()) - ScreenUtils.dip2px((20 * 2).toFloat())) / 4
-            para.width = (screenWidth - ScreenUtils.dip2px((15 * 2).toFloat()) - ScreenUtils.dip2px((20 * 2).toFloat())) / 4
-            helper.itemView.layoutParams = para
+        para.width = ((screenWidth - ScreenUtils.dip2px((15 * 2).toFloat()) - ScreenUtils.dip2px((20 * 2).toFloat())) / 4) - 6
+        helper.itemView.layoutParams = para
 
-            helper.setText(R.id.cb_item_select_card_bag, item.title)
-            helper.setChecked(R.id.cb_item_select_card_bag, item.isChecked)
+        helper.setText(R.id.cb_item_select_card_bag, item.title)
+        helper.setChecked(R.id.cb_item_select_card_bag, item.isChecked)
+        var checkBox = helper.getView<CheckBox>(R.id.cb_item_select_card_bag)
+
+        if (item.isChecked){
+            checkBox.setBackgroundResource(R.drawable.select_label)
+            helper.setTextColor(R.id.cb_item_select_card_bag,mContext.getColorForKotlin(R.color.white))
+        }else{
+            checkBox.setBackgroundResource(R.drawable.select_unlabel)
+            helper.setTextColor(R.id.cb_item_select_card_bag,mContext.getColorForKotlin(R.color.color_checkbox_text_select_attention))
+        }
+
 //        } else {
 //            helper.getView<FrameLayout>(R.id.fl_item_select_card_bag).visibility = View.GONE
 //        }
